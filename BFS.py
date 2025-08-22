f0866442887ba8ca44e26fdd41481e458bd389aa
CristianCative <cristiancative@usantotomas.edu.co>
2025-08-22 09:27:54 -0500

    Add files via upload
    Ejercicio 1 de BFS

---
+++ BFS.py
@@ -0,0 +1,42 @@
+from collections import deque
+
+def bfs(graph, start, goal):
+    visited = set()  # Set to keep track of visited nodes
+    queue = deque([[start]])  # Queue for BFS, starting with the initial node
+
+    if start == goal:
+        return "Start and goal nodes are the same"
+
+    while queue:
+        path = queue.popleft()  # Get the first path from the queue
+        node = path[-1]         # Get the last node from the path
+
+        if node not in visited:
+            neighbors = graph[node]  # Get neighbors of the current node
+
+            for neighbor in neighbors:
+                new_path = list(path)       # Copy the current path
+                new_path.append(neighbor)   # Add the neighbor to the path
+                queue.append(new_path)      # Add new path to the queue
+
+                if neighbor == goal:
+                    return new_path  # If neighbor is the goal, return the path
+
+            visited.add(node)  # Mark the node as explored
+
+    return "No path found between start and goal"
+
+
+# Example graph represented as an adjacency list
+graph = {
+    'A': ['B', 'L'],
+    'B': ['C', 'X'],
+    'C': ['E'],
+    'E': ['Z'],
+    
+}
+
+
+start_node = 'A'
+end_node = 'Z'
+print("BFS Path:", bfs(graph, start_node, end_node))
